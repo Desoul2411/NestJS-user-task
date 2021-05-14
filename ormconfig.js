ebd21d706b92cb8e9f52638b8aa2f0df5c14e12c
Dzmitry Shauchonak <dzmitry_shauchonak@epam.com>
2021-05-14 16:58:48 +0300

    fix: fixed docker-compose config

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,13 @@
 module.exports = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3307,
-    username: 'root',
-    password: '1',
-    database: 'user_schema',
-    dropSchema: true,
-    logging: false,
-    synchroize: true,
-    migrationsRun: true,
-    entities: ['src/user/user.entity.ts'],
-
-  };
+  type: "mysql",
+  host: "mysql",
+  // port: 3307,
+  username: "root",
+  password: null,
+  database: "mysql",
+  dropSchema: true,
+  logging: false,
+  synchroize: true,
+  migrationsRun: true,
+  entities: ["src/user/user.entity.ts"],
+};
